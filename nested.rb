be4618b8d4a5103f89dc3d34d334267983080843
Richard Chea <rchea817@gmail.com>
2020-03-24 23:13:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-	programmer_hash[:dennis_ritchie][:languages].merge!(["Assembly"])
+	programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
 	return programmer_hash
 end
