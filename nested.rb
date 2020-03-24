e5ac40b74350b34ad0543436184bce086c842f36
Richard Chea <rchea817@gmail.com>
2020-03-24 22:44:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+		programmer_hash[:alan_kay][:known_for] = "GUI"
+		return programmer_hash
 end
 
 def adding_to_dennis
@@ -128,6 +129,5 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-	programmer_hash[:alan_kay][:known_for] = "GUI"
-	return programmer_hash
+
 end
